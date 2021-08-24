e52964b6f52f7b6034cfcb7072dcac1022be9fc0
Adam Oneil <adamsoftware@gmail.com>
2021-08-23 20:05:49 -0400

    added SaveAsync method

--- Dapper.Repository.SqlServer/Extensions/SqlServerExtensions.cs
+++ Dapper.Repository.SqlServer/Extensions/SqlServerExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Dapper.Repository.Extensions;
+﻿using AO.Models.Interfaces;
+using Dapper.Repository.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -8,16 +9,30 @@ namespace Dapper.Repository.SqlServer.Extensions
 {
     public static class SqlServerExtensions
     {
-        public static async Task<TModel> GetAsync<TModel, TKey>(this IDbConnection connection, TKey id, string identityColumn = CrudExtensionsBase.IdentityColumn) => 
-            await CrudExtensionsBase.GetAsync<TModel, TKey>(connection, id, '[', ']', identityColumn);
+        private const char StartDelimiter = '[';
+        private const char EndDelimiter = ']';
 
-        public static async Task<TModel> InsertAsync<TModel, TKey>(this IDbConnection connection, TModel model, IEnumerable<string> columnNames = null, string identityColumn = "Id", Action<TModel, TKey> afterInsert = null) =>
-            await CrudExtensionsBase.InsertAsync(connection, model, '[', ']', columnNames, identityColumn, "SELECT SCOPE_IDENTITY();", afterInsert);
+        public static async Task<TModel> GetAsync<TModel, TKey>(this IDbConnection connection, TKey id, string identityColumn = CrudExtensionsBase.IdentityColumn) => 
+            await CrudExtensionsBase.GetAsync<TModel, TKey>(connection, id, StartDelimiter, EndDelimiter, identityColumn);
+
+        public static async Task<TModel> InsertAsync<TModel, TKey>(this IDbConnection connection, TModel model, IEnumerable<string> columnNames = null, string identityColumn = CrudExtensionsBase.IdentityColumn, Action<TModel, TKey> afterInsert = null) =>
+            await CrudExtensionsBase.InsertAsync(connection, model, StartDelimiter, EndDelimiter, columnNames, identityColumn, "SELECT SCOPE_IDENTITY();", afterInsert);
 
         public static async Task UpdateAsync<TModel>(this IDbConnection connection, TModel model, IEnumerable<string> columnNames = null, string identityColumn = CrudExtensionsBase.IdentityColumn) =>
-            await CrudExtensionsBase.UpdateAsync(connection, model, '[', ']', columnNames, identityColumn);
+            await CrudExtensionsBase.UpdateAsync(connection, model, StartDelimiter, EndDelimiter, columnNames, identityColumn);
 
         public static async Task DeleteAsync<TModel, TKey>(this IDbConnection connection, TKey id, string identityColumn = CrudExtensionsBase.IdentityColumn, string tableName = null) =>
-            await CrudExtensionsBase.DeleteAsync<TModel, TKey>(connection, id, '[', ']', identityColumn, tableName);
+            await CrudExtensionsBase.DeleteAsync<TModel, TKey>(connection, id, StartDelimiter, EndDelimiter, identityColumn, tableName);
+
+        public static async Task<TModel> SaveAynsc<TModel, TKey>(this IDbConnection connection, TModel model, IEnumerable<string> columnNames, string identityColumn = CrudExtensionsBase.IdentityColumn) where TModel : IModel<TKey>
+        {
+            if (model.Id.Equals(default(TKey)))
+            {
+                return await InsertAsync<TModel, TKey>(connection, model, columnNames, identityColumn, afterInsert: (row, id) => row.Id = id);
+            }
+
+            await UpdateAsync(connection, model, columnNames, identityColumn);
+            return model;
+        }
     }
 }
